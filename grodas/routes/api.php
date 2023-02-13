96e6e93289fa2ad4e6b56fbb4d554a9ca38e0202
ILIA ZOTOV <zotow.work@gmail.com>
2023-02-13 09:14:27 +0200

    I made routes for REST API

--- grodas/routes/api.php
+++ grodas/routes/api.php
@@ -17,3 +17,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/products', 'App\Http\Controllers\ProductsController@index');
+Route::get('/products/{id}', 'App\Http\Controllers\ProductsController@show');
+Route::put('/products/{id}', 'App\Http\Controllers\ProductsController@update');
+Route::delete('/products/{id}', 'App\Http\Controllers\ProductsController@destroy');
+Route::post('/orders', 'App\Http\Controllers\OrdersController@store');
+Route::get('/orders', 'App\Http\Controllers\OrdersController@index');
+Route::get('/orders/{id}', 'App\Http\Controllers\OrdersController@show');
+Route::put('/orders/{id}', 'App\Http\Controllers\OrdersController@update');
+Route::delete('/orders/{id}', 'App\Http\Controllers\OrdersController@destroy');
+Route::get('/categories', 'App\Http\Controllers\CategoriesController@index');
+Route::get('/categories/{id}', 'App\Http\Controllers\CategoriesController@show');
+Route::put('/categories/{id}', 'App\Http\Controllers\CategoriesController@update');
+Route::delete('/categories/{id}', 'App\Http\Controllers\CategoriesController@destroy');
+
